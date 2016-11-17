02200f10cd140c6a6b8fd2321545ddce9f8f360a
elizabethvenner <elizabethvenner@googlemail.com>
2016-11-17 16:56:29 +0000

    install devise - still a bug

--- yelp_clone/config/routes.rb
+++ yelp_clone/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :restaurants do
     resources :reviews
   end
@@ -8,6 +9,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'restaurants#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
